575cddc50289fb2c72118f482c03267cce9cbfb3
paulshu <paulshu18@gmail.com>
2017-05-28 00:15:17 +0800

    To optimize the products & product order / carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,43 @@ $(document).ready(function() {
     $('#myCarousel').carousel({interval: 4000})
     $(window).trigger('scroll') // 一开始就触发一下滚动事件
 });
+
+// 点击事件
+$(document).on('click', '.backtop', function () {
+  $('body').animate({'scrollTop': 0}, 500) // 在500ms内，慢慢地回到顶部,首页侧边栏分类中的箭头
+}).on('mouseenter', '.support', function () {
+  $('.ewm').show().stop().animate({left: '-136px', opacity: 1}, 500)
+}).on('mouseleave', '.support', function () {
+  $('.ewm').stop().animate({opacity: 0}, 500, function () {
+    $(this).css('left', 0).hide()
+  })
+})
+
+// 下拉时显示「顶部导航栏」和「侧边栏」
+var $header = $('.header')  // 可以放置搜索,购物车等
+var $sidebar = $('#sidebar')
+var $category_sidebar = $('.category_sidebar')
+var $lp_navbar = $('.lp-navbar')
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+    $sidebar.fadeIn()
+    $category_sidebar.fadeIn() // 慢慢地显示侧边栏
+  } else {
+    $sidebar.fadeOut()
+    $category_sidebar.fadeOut() // 慢慢地隐藏侧边栏
+  }
+
+  if ($(this).scrollTop() > 120) {
+    if ($header.is(':animated')) {
+      return false
+    }
+    $('.header_placeholder').show() // 显示「用来占位」的header
+    $header.addClass('header_fixed') // 让导航栏固定在顶部
+    $header.stop().animate({top: 0}, 600) // 在600ms内，慢慢地出来
+  } else {
+    $('.header_placeholder').hide()
+    $header.removeClass('header_fixed')
+    $header.css({top: -80})
+  }
+})
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 class ProductsController < ApplicationController
   def index
     if params[:category].blank?
-      @products = Product.all
+      @products = case params[:order]
+      when 'by_product_price'
+          Product.all.order("price DESC")
+      when 'by_product_quantity'
+        Product.all.order("quantity DESC")
+      else
+        Product.all.recent
+      end
+      
     else
       @category_id = Category.find_by(name: params[:category]).id
       @products = Product.where(category_id:  @category_id)

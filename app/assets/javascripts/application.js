01f613ebacb8ac02b2f746fcdf197d2b71a82e2f
paulshu <paulshu18@gmail.com>
2017-05-27 20:47:27 +0800

    update products page for display images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap-sprockets
 //= require autosize.min
 //= require jquery.countdown.min
 //= require wow.min

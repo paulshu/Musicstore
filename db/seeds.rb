6c8fd91930d27b26f6fea9b8417f731c181fd07a
paulshu <paulshu18@gmail.com>
2017-05-30 13:19:51 +0800

    test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,74 +6,133 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Initialize Admin User
-if User.find_by(email: "admin@123.com").nil?
+if User.find_by(email: "122380981@qq.com").nil?
   u = User.new
-  u.email = "admin@123.com"           # 可以改成自己的 email
-  u.password = "123456"                # 最少要六码
-  u.password_confirmation = "123456"   # 最少要六码
+  u.email = "122380981@qq.com"           # 可以改成自己的 email
+  u.password = "111111"                # 最少要六码
+  u.password_confirmation = "111111"   # 最少要六码
   u.is_admin = true
   u.save
   puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
 else
   puts "Admin 已经建立过了，脚本跳过该步骤。"
 end
+  f_title = [
+    "北京烤鸭",
+    "大闸蟹",
+    "东坡肉",
+    "剁椒鱼头",
+    "法式鸡公煲",
+    "宫保鸡丁",
+    "锅包肉",
+    "过桥米线",
+    "煎饼果子",
+    "麻辣香锅",
+    "墨西哥肉卷",
+    "三明治",
+    "三文鱼扒 ",
+    "水煮鱼",
+    "小笼包",
+    "红烧肉"
+  ]
+f_images = [
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrq4hl3blj30rs0jqdjg.jpg",
+  "http://ww2.sinaimg.cn/large/006tNbRwgy1ffrqa6g8lbj30i80r540z.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqaet2crj308c0693yt.jpg",
+  "http://ww3.sinaimg.cn/large/006tNbRwgy1ffrqalyuc6j30dw09bgm7.jpg",
+  "http://ww3.sinaimg.cn/large/006tNbRwgy1ffrqarbpb1j30dc06odgw.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqb1t147j30r80kfdkw.jpg",
+  "http://ww2.sinaimg.cn/large/006tNbRwgy1ffrqb83ynaj30hs0a3diy.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqbeb3lgj30b408ctd5.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqbtofakj30c807sdgi.jpg",
+  "http://ww3.sinaimg.cn/large/006tNbRwgy1ffrqceyovbj30m80etgx2.jpg",
+  "http://ww1.sinaimg.cn/large/006tNbRwgy1ffrqcrtif5j30h40cu75f.jpg",
+  "http://ww1.sinaimg.cn/large/006tNbRwgy1ffrqd72vprj30p00e2my3.jpg",
+  "http://ww3.sinaimg.cn/large/006tNbRwgy1ffrqdgbtptj30ec09k42j.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqdo4poej30dw099wfq.jpg",
+  "http://ww1.sinaimg.cn/large/006tNbRwgy1ffrqe008olj30le0c8q9k.jpg",
+  "http://ww4.sinaimg.cn/large/006tNbRwgy1ffrqbloodsj30p00gowj0.jpg",
+]
+    for i in 0..15 do
+      Product.create!(
+      title: f_title[i],
+      description:"这只是底料，请去实验室DIY！",
+      quantity:rand(6..10),
+      price:rand(50..100)*10,
+      image: open(f_images[i]),
+      )
+      puts i
+    end
 
 
-# product categroy
-  Category.create!(name: "键盘乐器")
-  Category.create!(name: "吉他贝斯")
-  Category.create!(name: "录音设备")
-  Category.create!(name: "管弦乐器")
-  Category.create!(name: "打击乐器")
-
-
-# Initialize Product
-
-Product.create!(
-  id: 1,
-  category_id: 1,
-  title: "YAMAHA 雅马哈 P-115B全套88键数码钢琴",
-  description: "本款钢琴仍然采用德国优秀的防锈钢线,UP119QS采用新材料，新工艺进行限量生产，精细加工。",
-  price: 4199,
-  quantity: 105,
-  # image: MiniMagick::Image.open("") ##details
-)
-
-Photo.create!(product_id:1, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-101.jpg"))
-Photo.create!(product_id:1, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-102.jpg"))
-
-
-Product.create!(
-  id: 2,
-  category_id: 1,
-  title: "XINGHAI 星海钢琴 XU-23JA 立式家用",
-  description: "初学专业演奏立式 黑色原声钢琴 供应商直送",
-  price: 16800,
-  quantity: 10001,
-  # image: MiniMagick::Image.open("") ##details
-)
-
-Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-201.jpg"))
-Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-202.jpg"))
-Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-203.jpg"))
-Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-204.jpg"))
-Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-205.jpg"))
-
-Product.create!(
-  id: 3,
-  category_id: 1,
-  title: "PEARLRIVER 钢琴 家用立式钢琴UP119QS",
-  description: "采用德国优秀的防锈钢线,限量生产，精细加工",
-  price: 17300,
-  quantity: 10001,
-  # image: MiniMagick::Image.open("") ##details
-)
-
-Photo.create!(product_id:3, image: open("https://ws2.sinaimg.cn/large/006tNbRwgy1fg2alyu0xaj30rs0rsabe.jpg"))
-Photo.create!(product_id:3, image: open("https://ws3.sinaimg.cn/large/006tNbRwgy1fg2am6n34xj30m80m8wfg.jpg"))
-Photo.create!(product_id:3, image: open("https://ws1.sinaimg.cn/large/006tNbRwgy1fg2amdc0ugj30rs0rswh4.jpg"))
-Photo.create!(product_id:3, image: open("https://ws3.sinaimg.cn/large/006tNbRwgy1fg2amlv11xj30rs0rsdhq.jpg"))
+# Initialize Admin User
+# if User.find_by(email: "admin@123.com").nil?
+#   u = User.new
+#   u.email = "admin@123.com"           # 可以改成自己的 email
+#   u.password = "123456"                # 最少要六码
+#   u.password_confirmation = "123456"   # 最少要六码
+#   u.is_admin = true
+#   u.save
+#   puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
+# else
+#   puts "Admin 已经建立过了，脚本跳过该步骤。"
+# end
+#
+#
+# # product categroy
+#   Category.create!(name: "键盘乐器")
+#   Category.create!(name: "吉他贝斯")
+#   Category.create!(name: "录音设备")
+#   Category.create!(name: "管弦乐器")
+#   Category.create!(name: "打击乐器")
+#
+#
+# # Initialize Product
+#
+# Product.create!(
+#   id: 1,
+#   category_id: 1,
+#   title: "YAMAHA 雅马哈 P-115B全套88键数码钢琴",
+#   description: "本款钢琴仍然采用德国优秀的防锈钢线,UP119QS采用新材料，新工艺进行限量生产，精细加工。",
+#   price: 4199,
+#   quantity: 105,
+#   # image: MiniMagick::Image.open("") ##details
+# )
+#
+# Photo.create!(product_id:1, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-101.jpg"))
+# Photo.create!(product_id:1, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-102.jpg"))
+#
+#
+# Product.create!(
+#   id: 2,
+#   category_id: 1,
+#   title: "XINGHAI 星海钢琴 XU-23JA 立式家用",
+#   description: "初学专业演奏立式 黑色原声钢琴 供应商直送",
+#   price: 16800,
+#   quantity: 10001,
+#   # image: MiniMagick::Image.open("") ##details
+# )
+#
+# Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-201.jpg"))
+# Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-202.jpg"))
+# Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-203.jpg"))
+# Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-204.jpg"))
+# Photo.create!(product_id:2, image: open("http://oqqzffr5e.bkt.clouddn.com/2017-05-30-205.jpg"))
+#
+# Product.create!(
+#   id: 3,
+#   category_id: 1,
+#   title: "PEARLRIVER 钢琴 家用立式钢琴UP119QS",
+#   description: "采用德国优秀的防锈钢线,限量生产，精细加工",
+#   price: 17300,
+#   quantity: 10001,
+#   # image: MiniMagick::Image.open("") ##details
+# )
+#
+# Photo.create!(product_id:3, image: open("https://ws2.sinaimg.cn/large/006tNbRwgy1fg2alyu0xaj30rs0rsabe.jpg"))
+# Photo.create!(product_id:3, image: open("https://ws3.sinaimg.cn/large/006tNbRwgy1fg2am6n34xj30m80m8wfg.jpg"))
+# Photo.create!(product_id:3, image: open("https://ws1.sinaimg.cn/large/006tNbRwgy1fg2amdc0ugj30rs0rswh4.jpg"))
+# Photo.create!(product_id:3, image: open("https://ws3.sinaimg.cn/large/006tNbRwgy1fg2amlv11xj30rs0rsdhq.jpg"))
 
 # Product.create!(
 #   id: 4,

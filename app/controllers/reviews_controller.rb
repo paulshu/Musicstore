b864f96b8fbd7028814086e62928dad8c1642052
Lucia <learningleadtorebirth@gmail.com>
2017-06-05 17:54:08 +0800

    update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,51 +8,31 @@ class ReviewsController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
     @review = Review.new
-    @graphic = @review.graphics.build
   end
 
   def create
-    @product = Product.find(params[:product_id])
-    @review = Review.new(review_params)
-    @review.product = @product
-    @review.user = current_user
+  @product = Product.find(params[:product_id])
+  @review = Review.new(review_params)
+  @review.product = @product
+  @review.user = current_user
 
-    if @review.save
-      if params[:graphics] != nil
-        params[:graphics]['image'].each do |a|
-          @graphic  = @review.graphics.create!(:image => a)
-        end
+  if @review.save
+   if params[:graphics] != nil
+      params[:graphics]['image'].each do |a|
+        @graphic = @review.graphics.create(:image => a)
       end
-      redirect_to product_path(@product), notice: '评论已提交！'
-    else
-      render :new
     end
-
+    redirect_to product_path(@product)
+  else
+    render :new
   end
+end
 
-  def edit
-    @product = Product.find(params[:product_id])
-    @review = Review.find(params[:id])
-
-    if @review.update(review_params)
-      redirect_to product_path(@product), notice: '评论更新成功！'
-    else
-      render :edit
-    end
-  end
-
-  def destory
-    @product = Product.find(params[:product_id])
-    @review = Review.find(params[:id])
-    @review.destory
-    redirect_to product_path(@product), alert: '已成功删除评论'
-  end
 
   private
 
   def review_params
-    # params.require(:review).permit(:content, :rating, :look, :price, graphics_attributes: [:id, :review_id, :image])
-    params.require(:review).permit(:content, :rating, :look, :price)
+    params.require(:review).permit(:content, :rating, :look, :price, :image)
   end
 
 end

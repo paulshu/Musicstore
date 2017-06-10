cc8b92cec81b1d70dbc70e54e3647456da76c77f
paulshu <paulshu18@gmail.com>
2017-06-10 17:11:37 +0800

    alipay

---
+++ config/initializers/alipay.rb
@@ -0,0 +1,5 @@
+Alipay.pid = 'YOUR_PID'
+Alipay.key = 'YOUR_KEY'
+
+#Alipay.sign_type = 'MD5' # Available values: MD5, RSA. Default is MD5
+#Alipay.debug_mode = true # Enable parameter check. Default is true.
